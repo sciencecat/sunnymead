abfc2a3a55a86c41e1b9b6b5439c6483797b0d3e
sciencecat <sciencecat7@gmail.com>
2016-06-18 21:26:12 +0000

    adjust pdf builder and add html parser

--- app/www/src/services/document_builder.js
+++ app/www/src/services/document_builder.js
@@ -5,33 +5,42 @@
     .module('app')
     .factory('DocumentBuilderService', DocumentBuilder);
 
-  DocumentBuilder.$inject = [];
+  DocumentBuilder.$inject = ['HTMLToPDFParser'];
 
-  function DocumentBuilder() {
+  function DocumentBuilder(HTMLToPDFParser) {
     function create(result) {
-      console.log(result);
-      
       var documentDefinition = {
         pageSize: 'A4',
       	content: [
           h1('Resultado - 9Types'),
         	divisor(),
-        	h2('Maior incidência'),
-        	typeList(result.firstPlaces, 14),
-        	divisor(),
-        	h2('Segunda maior incidência'),
-        	typeList(result.secondPlaces, 12),
+          h2('Dados do usuário'),
+        	userData(),
         	divisor(),
         	h2('Pontuação'),
         	image(),
-        	divisor(),
+        	divisor('before'),
+        	h2('Maior incidência'),
+        	typeList(result.firstPlaces, 14),
+        	divisor('before'),
+        	h2('Segunda maior incidência'),
+        	typeList(result.secondPlaces, 12),
+        	divisor('before'),
         	h2('Menor incidência'),
-        	typeList(result.lastPlaces, 12, '#ef473a'),
+        	typeList(result.lastPlaces, 12),
       	]
       };
       
-      function divisor() {
-      	return { canvas: [{ type: 'line', x1: 60, y1: 10, x2: 455, y2: 10, lineWidth: 0, lineColor: '#CCC' }] };
+      function divisor(pageBreak) {
+      	var content = { canvas: [
+      	  { type: 'line', x1: 60, y1: 10, x2: 455, y2: 10, lineWidth: 0, lineColor: '#CCC' }
+      	]};
+      	
+        if (pageBreak) {
+          content.pageBreak = pageBreak;
+        }
+      	
+      	return content;
       }
       
       function h1(text) {
@@ -42,25 +51,56 @@
         return { text: text, fontSize: 10, alignment: 'center', margin: [ 0, 5, 0, 10 ], color: '#AAA' };
       }
       
-      function typeItem(item, size, color) {
-        console.log(item);
+      function userData() {
+        return {
+          columns: [
+            { width: 110, text: '' },
+            {
+              width: 330,
+              table: {
+                body: [
+                  [
+                    { text: 'Nome:', fontSize: 10, bold: true, alignment: 'right' },
+                    { text: result.user.name, fontSize: 10, alignment: 'left' }
+                  ],
+                  [
+                    { text: 'Email:', fontSize: 10, bold: true, alignment: 'right' },
+                    { text: result.user.email, fontSize: 10, alignment: 'left' }
+                  ]
+                ]
+              },
+              layout: {
+                hLineWidth: function() { return 0; },
+                vLineWidth: function() { return 0; },
+                paddingLeft: function() { return 10; },
+                paddingRight: function() { return 10; },
+                paddingTop: function() { return 5; },
+                paddingBottom: function() { return 5; }
+              }
+            },
+            { width: 80, text: '' }
+          ]
+        };
+      }
+      
+      function typeItem(item, size) {
         return [
           [
-            { text: 'Tipo', fontSize: size, alignment: 'center', color: color || '#000' },
-            { text: String(item.type), fontSize: size * 2, alignment: 'center', color: color || '#000' }
+            { text: 'Tipo', fontSize: size, alignment: 'center' },
+            { text: String(item.type), fontSize: size * 2, alignment: 'center' }
           ],
-          { text: item.description, fontSize: size * 0.75, color: color || '#000' }
+          HTMLToPDFParser.parse(item.details)
         ];
       }
       
-      function typeList(types, size, color) {
+      function typeList(types, size) {
         return {
           columns: [
             { width: 70, text: '' },
             {
               width: 400,
               table: {
-                body: types.map(function (type) { return typeItem(type, size, color); })
+                body: types.map(function (type) { return typeItem(type, size); })
               },
               layout: {
                 hLineWidth: function() { return 0; },
@@ -79,8 +119,8 @@
       function image() {
         return {
             image: result.graphImageURL,
-            width: 300,
-            height: 300,
+            width: 360,
+            height: 360,
             alignment: 'center'
       	};
       }
